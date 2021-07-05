8ec8e5dbbd7b4850fb78beec78f50d83a3ba6fea
Nishant <Nishant2548@gmail.com>
2021-07-05 15:00:35 +0530

    Completed

--- app/src/main/java/com/example/bookk/Adapter/BorrowedAdapter.kt
+++ app/src/main/java/com/example/bookk/Adapter/BorrowedAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.bookk.Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.bookk.Callbacks.BorrowedCallback
@@ -49,6 +52,13 @@ class BorrowedAdapter(val context: Context?, var list:ArrayList<BorrowedModel>, 
             name.text = borrow.Name
             genre.text = borrow.Genre
             publishYear.text = "Published in ${borrow.Release}"
+
+            reBody.findViewById<CardView>(R.id.openBook).setOnClickListener({
+                var intent = Intent(Intent.ACTION_VIEW);
+                intent.setType("application/pdf")
+                intent.setData(Uri.parse("https://firebasestorage.googleapis.com/v0/b/bookk-e6f58.appspot.com/o/Books%2Fthe_kite_runner.pdf?alt=media&token=ee11e23b-42e9-4c21-84a9-77f059b2210f"))
+                context?.startActivity(intent);
+            })
 
             reBody.setOnClickListener {
                 var edit: SharedPreferences.Editor? =
